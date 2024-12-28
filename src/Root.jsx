302fe5f46ea11d7e402b1205d98296d90745af92
suthyscott <sutherlandscott21@gmail.com>
2024-12-27 19:32:13 -0700

    home page done

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import NavigationBar from "./components/NavigationBar.jsx";
 import { Outlet } from "react-router-dom";
 
 const Root = () => {
   return (
-    <>
-      <NavigationBar />
       <Outlet />
-    </>
   );
 };
 
